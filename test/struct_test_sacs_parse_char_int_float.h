b891707174ff34a8633c4d6ae7946fa5430319e9
Joshua Petitt <joshpetitt@jpmec.com>
2013-02-10 21:49:11 -0600

    Added test files

---
+++ test/struct_test_sacs_parse_char_int_float.h
@@ -0,0 +1,27 @@
+
+#ifndef TESTSACSSTRUCTPARSECHARINTFLOAT_H
+
+
+#include <stdio.h>
+
+#include "../src/sacs.h"
+
+
+
+
+#undef SACS_STRUCT
+#define SACS_STRUCT struct_test_sacs_parse_char_int_float
+
+#include "../src/sacs_struct.h"
+#include "struct_test_sacs_parse_char_int_float.sac"
+
+#include "../src/sacs_parsable.h"
+#include "struct_test_sacs_parse_char_int_float.sac"
+
+#include "../src/sacs_snprintfable.h"
+#include "struct_test_sacs_parse_char_int_float.sac"
+
+
+
+
+#endif
